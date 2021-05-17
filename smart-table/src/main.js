a076d6a5150ddd8c87332a5b81f0eb2e875f80e5
lotostoii <lotostoii@gmail.com>
2021-05-17 16:12:30 +1000

    first screen without soket.io

--- smart-table/src/main.js
+++ smart-table/src/main.js
@@ -3,4 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import Vue3TouchEvents from "vue3-touch-events";
+
+
+createApp(App).use(store).use(router).use(Vue3TouchEvents).mount("#app");
--- smart-table/src/views/Home.vue
+++ smart-table/src/views/Home.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="wrapper">
-    <div class="content">
-      <div class="img num1">
-        <img src="@/assets/images/Thumb1.jpg" class="img" alt="img" />
-      </div>
-      <div class="img num2">
+    <div
+      class="content"
+      @dragstart="dragOff"
+      :style="'padding-top: ' + dropAreaBottom + 'px;'"
+    >
+      <div class="dropArea" ref="dropArea"></div>
+      <div
+        class="img num1"
+        v-for="img in content"
+        :key="img.url"
+        :style="'width: ' + size.width + 'px; height: ' + size.height + 'px;'"
+      >
         <img
-          src="@/assets/images/Thumb1.jpg"
+          :style="'width: ' + size.width + 'px; height: ' + size.height + 'px;'"
+          :src="img.url"
           class="img"
           alt="img"
-          @drag="move($event)"
-          @dragend="end($event)"
+          :ref="img.ref"
+          v-touch:press="start()"
+          v-touch:drag="move()"
+          v-touch:release="() => drop()"
+          @mousemove="() => {}"
+          :draggable="false"
         />
-      </div>
-      <div class="img num3">
-        <img src="@/assets/images/Thumb1.jpg" class="img" alt="img" />
       </div>
     </div>
   </div>
@@ -25,16 +34,23 @@ export default {
   data() {
     return {
       content: [
-        {
-          tag: "video",
-          url: require("@/assets/videos/first.mov"),
-          ref: "first",
-        },
+        { url: require("@/assets/images/Thumb1.jpg"), ref: "img1" },
+        { url: require("@/assets/images/Thumb2.jpg"), ref: "img2" },
+        { url: require("@/assets/images/Thumb3.jpg"), ref: "img3" },
       ],
-      startValue: { x: null, y: null },
+      element: null,
+      dropAreaBottom: 100,
+      size: {},
     };
   },
-  mounted() {},
+  mounted() {
+    this.dropAreaBottom = (document.documentElement.clientHeight * 15) / 100;
+    const startWidth = (document.documentElement.clientWidth * 25) / 100;
+    this.size = {
+      width: startWidth > 200 ? startWidth : 200,
+      height: (startWidth > 200 ? startWidth : 200) / 1.5,
+    };
+  },
   methods: {
     getCoords(elem) {
       let box = elem.getBoundingClientRect();
@@ -43,29 +59,37 @@ export default {
         left: box.left + pageXOffset,
       };
     },
-    move(e) {
-      const el = e.target;
-      el.style.cssText = "position:absolute";
-      if (this.startValue.x === null) {
-        this.startValue = {
-          x: el.left,
-          y: el.top,
-        };
-      }
-      console.log(el.left);
-
-      el.style.left = e.clientX - el.offsetWidth / 2 + "px";
-      el.style.top = e.clientY - el.offsetHeight / 2 + "px";
+    dragOff() {
+      return false;
     },
-    end(e) {
-      console.log("test");
-      const el = e.target;
-      el.style.left = this.startValue.x + "px";
-      el.style.top = this.startValue.y + "px";
-      this.startValue = {
-        x: null,
-        y: null,
+    start() {
+      return (e) => {
+        this.element = {};
+        this.element.el = e.target;
+        this.element.x = this.getCoords(this.element.el).left;
+        this.element.y = this.getCoords(this.element.el).top;
       };
+    },
+    move() {
+      return (e) => {
+        console.log(e);
+        if (!this.element) return;
+        this.element.el.style.position = "absolute";
+        this.element.el.style.zIndex = 1000;
+        this.element.el.style.left =
+          e.changedTouches[0].pageX - this.element.el.offsetWidth / 2 + "px";
+        this.element.el.style.top =
+          e.changedTouches[0].pageY - this.element.el.offsetHeight / 2 + "px";
+        if (e.changedTouches[0].pageY < this.dropAreaBottom / 1.5) this.drop();
+      };
+    },
+    drop() {
+      if (!this.element) return;
+      this.element.el.style.left = this.element.x + "px";
+      this.element.el.style.top = this.element.y + "px";
+      this.element.el.style.position = "static";
+      this.element.el.style.zIndex = 10;
+      this.element = null;
     },
   },
 };
@@ -82,6 +106,14 @@ export default {
   background-color: black;
   position: relative;
 }
+.dropArea {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 15vh;
+  border: 1px solid gray;
+}
 .content {
   position: absolute;
   top: 0;
@@ -91,6 +123,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-wrap: wrap;
   & > .img {
     width: 400px;
     height: 280px;
