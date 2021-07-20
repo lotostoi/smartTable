9317ce700391ccf9de77297289979bf2f488570a
lotostoii <lotostoii@gmail.com>
2021-07-20 13:43:43 +1000

    corrected  connect to bd

--- smart-table/src/server/src/lib/connect-db.js
+++ smart-table/src/server/src/lib/connect-db.js
@@ -5,7 +5,7 @@ module.exports = async function ({ login, password, nameDataBase }, cb) {
   try {
     const link =
       process.env.NODE_NODE_ENV !== "development"
-        ? `mongodb://root:FyWRHgMN0jun8Bq@127.0.0.1:27017/`
+        ? `mongodb://root:FyWRHgMN0jun8Bq@127.0.0.1:27017/${nameDataBase}?authSource=admin`
         : `mongodb+srv://${login}:${password}@cluster0.n6oze.mongodb.net/${nameDataBase}?retryWrites=true&w=majority`;
     const db = await mongoose.connect(link, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Connection to db is success...");
